69164bc297f40b5c455db253c90012cfcec6f370
Charles Clemons <Charles@charless-macbook-pro.local>
2017-02-06 14:23:07 -0500

    Initial Commit

---
+++ Clemons_UnitConverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Clemons_UnitConverter
+//
+//  Created by Charles Clemons on 2/6/17.
+//  Copyright © 2017 University of Cincinnati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
