adb53639b627ec3753d5b1ea57547cea4e1ae236
Charles Clemons <Charles@charless-macbook-pro.local>
2017-02-14 09:50:14 -0500

    UnitConverter

--- Clemons_UnitConverter/ViewController.swift
+++ Clemons_UnitConverter/ViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    var units: NSDictionary!
+    var unitName: NSArray!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // 1. get a reference to the app bundle
+        let appBundle = Bundle.main
+        //2. get a reference to the units file path
+        let filePath = appBundle.path(forResource: "Units", ofType: "plist")!
+        //3. load the file into the units dictionary object
+        units = NSDictionary(contentsOfFile: filePath)!
+        //4. retrieve the unit names(keys) into the unitNames array object
+        unitName = units.allKeys as NSArray!
     }
-
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return unitName.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return unitName.object(at: row) as? String
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
